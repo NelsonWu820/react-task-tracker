6408b123f07834614ee9bff611425ebb1bedbff2
Nelson Wu <Nelsonwu820@gmail.com>
2023-03-16 18:14:16 -0400

    adding components

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,11 @@
+import Button from "./Button"
+
 const Header = () => {
   return (
-    <div>Task Tracker</div>
+    <header className="header">
+        <h1>Task Tracker</h1>
+        <Button color='black' text="Click Me!"/>
+    </header>
   )
 }
 
