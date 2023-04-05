0084523f91eb8a22352127727fde2d08a864bfdd
Nelson Wu <Nelsonwu820@gmail.com>
2023-04-05 13:46:18 -0400

    Adding text to btn

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <section className="header">
         <h1>Task Tracker</h1>
-        <Button color='black' text="Click Me!"/>
+        <Button color='black' text="Add Task"/>
     </section>
   )
 }
