8aac2da2158223db1f533daa55be828b8e2ba687
Nelson Wu <Nelsonwu820@gmail.com>
2023-04-06 08:48:09 -0400

    adding an add task func

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import Header from './components/Header';
 import AddTasks from './components/AddTasks';
 import Tasks from './components/Tasks';
+import { useState } from 'react';
 
 function App() {
+  const [tasks, setTasks] = useState([]) 
+  
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1
+    const newTask = { id, ...task}
+    setTasks([...tasks, newTask])
+  }
+
+  const onDelete = () => { 
+
+  }
   return (
     <div className="App">
       <Header/>     
       <AddTasks/>
-      <Tasks/>
+      <Tasks onDelete={onDelete} task={tasks}/>
     </div>
   );
 }
